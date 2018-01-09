5977b13b664a0e927fe2f82f2f02bffc1a5cdebc
Xoberti1 <xoberti@gmail.com>
2018-01-08 21:12:11 -0600

    herokustuff

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// // Make connection.
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
-// Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
