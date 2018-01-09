c937d907d47223cd9d7037b6dfc00335645c38a5
Xoberti1 <xoberti@gmail.com>
2018-01-08 20:14:56 -0600

    working

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES('jalapeno', false, current_timestamp);
+
+INSERT INTO burgers
+    (burger_name, devoured, date)
+VALUES('swiss', false, current_timestamp);
+
+INSERT INTO burgers
+    (burger_name, devoured, date)
+VALUES('mushroom', false, current_timestamp);
